b11c17628f7a63728639a23ea74a1eb9741bc893
Joe Basquez <jybasquez@yahoo.com>
2021-03-30 07:43:50 -0400

    added export of resistance

--- models/resistance.js
+++ models/resistance.js
@@ -18,4 +18,6 @@ const Resistance = new Resistance({
     // resistance-duration:{
     //     type: Number
     // }
-})
+});
+const Resistance = mongoose.model("Resistance", ResistanceSchema);
+module.exports = Resistance;
