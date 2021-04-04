5f469278b536e8760033cd04ff85a195eb54ee1a
Joe Basquez <jybasquez@yahoo.com>
2021-04-04 18:17:52 -0400

    added to server.js files including routes and added routes folder with api.js and htmlRoutes.js

--- develop/server.js
+++ develop/server.js
@@ -1,9 +1,28 @@
-const exports = require("express");
+const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+//const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+//app.use(require("../routes/api.js"));
+//app.use(require("../routes/htmlRoute.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });  
 //create
 
 //get all///
