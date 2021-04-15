bc2fb3831c60f39480f33d12a3885d53f5727384
Joe Basquez <jybasquez@yahoo.com>
2021-04-15 19:07:28 -0400

    cleaning code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,9 @@
 
 var path = require('path');
 module.exports = function(app){
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,"develop/public/index.html"));
+  })
   app.get('/exercise', function (req, res) {
     res.sendFile(path.join(__dirname,"develop/public/exercise.html"));
   })
