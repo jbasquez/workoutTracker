cda814ba5b7cea79c53df00432b431b18964d9c7
Joe Basquez <jybasquez@yahoo.com>
2021-04-16 20:20:25 -0400

    working on schema

--- models/workout.js
+++ models/workout.js
@@ -2,33 +2,50 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    type:{
-        type: String,
-        required: "enter a excersize type"
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date
+        },
+        excercises: [
+            {
+                type:{
+                    type: String,
+                    required: "enter a excersize type"
+                },
+                name:{
+                    type: String,
+                    required: "enter a workout name"
+                },
+                distance:{
+                    type: Number,
+                    required: "enter a distance"
+                },
+                duration:{
+                    type: Number,
+                    required: "enter how long"
+                },
+                weight:{
+                    type: Number
+                },
+                reps:{
+                    type: Number
+                }
+            }
+        ]
+
     },
-    name:{
-        type: String,
-        required: "enter a workout name"
-    },
-    distance:{
-        type: Number,
-        required: "enter a distance"
-    },
-    duration:{
-        type: Number,
-        required: "enter how long"
-    },
-    weight:{
-        type: Number
-    },
-    reps:{
-        type: Number
+    {
+        toJSON:{
+            virtuals:true
+        }
     }
-});
+)
 
-const Excersize = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = Excersize;
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
 
 //https://mongoosejs.com/docs/tutorials/virtuals.html
